099110d746242bd7892079e8fd075b434f5cf4b3
Aarav619 <aaravmishra619@gmail.com>
2022-01-15 22:49:41 +0530

    Update file paths to restore

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,12 +1,12 @@
 #Restoration of my GDE
 
 #Full Restore
-dconf load / < full-backup
+dconf load / < ../full-backup
 
 # Gnome only settings Restore
 # dconf load /org/gnome < gnome-backup
 
-# echo -e "\nCompleted Gnome-Settings restoration..."
+echo -e "\nCompleted Gnome-Settings restoration..."
 
 #Restore system wide icons and themes
 # sudo tar --extract --file custom-icons.tar.gz -C /usr/share/ --strip-components=1 --overwrite 
@@ -14,21 +14,21 @@ dconf load / < full-backup
 # sudo tar --extract --file custom-themes.tar.gz -C /usr/share/ --strip-components=1 --overwrite
 
 #Restore .icons and .themes for "/" directory
-cp -r .icons /$HOME/
+cp -r ../.icons /$HOME/
 
-cp -r .themes /$HOME/
+cp -r ../.themes /$HOME/
 
 echo -e "\nCompleted icons and theme restoration..."
 
 #Restore Gnome-Extensions
-cp -r extensions  /$HOME/.local/share/gnome-shell/
+cp -r ../extensions  /$HOME/.local/share/gnome-shell/
 
 echo -e "\nCompleted Gnome-Extensions restoration..."
 
 #Restore .zshrc
-cp -r .zshrc /$HOME/
+cp -r ../.zshrc /$HOME/
 
 echo -e "\nCompleted .zshrc restoration..."
 
 #Restore Starship
-cp -r starship /$HOME/.config
+cp -r ../starship /$HOME/.config
