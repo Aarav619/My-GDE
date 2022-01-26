f57a70342cc448c5479b7209baca62dae75cf482
Aarav619 <aaravmishra619@gmail.com>
2022-01-26 17:21:33 +0530

    Ch success msg

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -13,4 +13,4 @@ sudo cp -r $backup_path/. $restore_path
 #changing ownership
 sudo chown -R $USER: $HOME
 
-echo -e "\n Syncing done... Don't panic now... Take a sigh of relief :)"
+echo -e "\n Syncing done... Take a sigh of relief :)"
